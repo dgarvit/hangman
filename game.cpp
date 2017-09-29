1d531884d5f1b88e247cf8ecd00abf52c4284b34
Garvit <d.garvit@gmail.com>
2017-09-30 00:54:41 +0530

    Add class linked list and select character from file

--- game.cpp
+++ game.cpp
@@ -1,13 +1,86 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+class linked_list
+{
+private:
+	struct node {
+		char data[100];
+		node* next;
+	};
+
+	node* head;
+
+	int size;
+
+public:
+	linked_list() {
+		size = 0;
+		head = NULL;
+	}
+
+	void push(char x[]) {
+		node* temp = new node;
+		strcpy(temp->data, x);
+		temp -> next = head;
+		head = temp;
+		size++;
+	}
+
+	void show() {
+		node* temp = head;
+		while (temp!=NULL) {
+			cout << temp -> data << "\n";
+			temp = temp -> next;
+		}
+	}
+
+	int getSize() {
+		return size;
+	}
+
+	char* get(int idx) {
+		node* temp = head;
+		int i = 0;
+		while (temp!=NULL && i < idx) {
+			temp = temp->next;
+			i++;
+		}
+
+		return temp->data;
+	}
+};
 
 class Hangman
 {
+private:
+	char* word;
+	linked_list words;
+	void getWord();
+
 public:
 	Hangman();
 	void rules();
 	char* beginGame();
 };
+
+void Hangman::getWord() {
+	int size = words.getSize();
+	if (words.getSize() == 0) {
+		ifstream fin("words.txt", ios::in);
+		char word[100];
+		while(!fin.eof()) {
+			fin >> word;
+			words.push(word);
+		}
+		fin.close();
+	}
+	size = words.getSize();
+	srand(time(NULL));
+	int x = rand()%size;
+	word = words.get(x);
+}
 
 Hangman::Hangman() {
 	cout << "Welcome! Press (1) to begin the game, press (2) to exit.";
@@ -19,9 +92,8 @@ Hangman::Hangman() {
 		case '1':
 			cout << "Let's begin!\n";
 			rules();
-			char *word = beginGame();
+			getWord();
 			cout << " \n\n" << word << "\n\n";
-			//core(word);
 			break;
 
 		case '2':
@@ -37,7 +109,7 @@ void Hangman::rules() {
 }
 
 char* Hangman::beginGame() {
-	return "word";
+
 }
 
 int main()
